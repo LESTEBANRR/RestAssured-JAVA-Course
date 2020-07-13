464e439467603ea2cb434b5460919cd348b3fd4a
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-13 13:40:24 -0500

    Clase 62

--- demo_oauth/src/demo/oAuthTest.java
+++ demo_oauth/src/demo/oAuthTest.java
@@ -11,7 +11,8 @@ import io.restassured.path.json.JsonPath;
 public class oAuthTest {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub		
+		/*
+		//Selenium Automation, Google don't want automation login.		
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("");
@@ -22,10 +23,21 @@ public class oAuthTest {
 		driver.findElement(By.cssSelector("input[type='password']")).sendKeys(Keys.ENTER);
 		Thread.sleep(3000);
 		String url=driver.getCurrentUrl();
+		String partialCode= url.split("code=")[1];
+		String code=partialCode.split("&scope")[0];
+		*/
 		
+		String url="https://rahulshettyacademy.com/getCourse.php?state=verifyfjdss"
+				+ "&code=4%2FvAHBQUZU6o4WJ719NrGBzSELBFVBI9XbxvOtYpmYpeV47bFVExkaxWaF_XR14PHtTZf7ILSEeamywJKwo_BYs9M"
+				+ "&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid"
+				+ "&authuser=0&session_state=0c32992f0d47e93d273922018ade42d1072b9d1f..a35c&prompt=none#";
+		
+		String partialCode= url.split("code=")[1];
+		String code=partialCode.split("&scope")[0];
+		// Get the Access Token
 		String accessTokenResponse=
-		given()
-		.queryParam("code", "")
+		given().urlEncodingEnabled(false)
+		.queryParam("code", code)
 		.queryParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
 		.queryParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
 		.queryParam("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
@@ -36,7 +48,7 @@ public class oAuthTest {
 		;
 		JsonPath js=new JsonPath(accessTokenResponse);
 		String accessToken=js.getString("access_token");
-		
+		// Login in the page
 		String response=
 		given()
 		.queryParam("access_token", accessToken)
@@ -45,7 +57,7 @@ public class oAuthTest {
 		.asString()
 		;
 
-		System.out.println(response);		
+		//System.out.println(response);		
 		
 	}
 
