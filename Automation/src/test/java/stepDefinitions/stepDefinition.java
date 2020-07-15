7200c43f2442b0fadb7d9b7ea1066d863e786ee9
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-15 15:58:02 -0500

    Clase 95

--- Automation/src/test/java/stepDefinitions/stepDefinition.java
+++ Automation/src/test/java/stepDefinitions/stepDefinition.java
@@ -12,22 +12,22 @@ public class stepDefinition {
 
 	@Given("^User is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
-        throw new PendingException();
+        System.out.println("Step 1");
     }
 
     @When("^User login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
-        throw new PendingException();
+    	System.out.println("Step 2");
     }
 
     @Then("^Home page is populated$")
     public void home_page_is_populated() throws Throwable {
-        throw new PendingException();
+    	System.out.println("Step 3");
     }
 
     @And("^Cards are displayed$")
     public void cards_are_displayed() throws Throwable {
-        throw new PendingException();
+    	System.out.println("Step 4");
     }
 
 }
