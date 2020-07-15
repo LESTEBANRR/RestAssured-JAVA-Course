791aff8b60552e00e48473aa42552be1fe1230f3
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-15 13:34:56 -0500

    Clase 93

---
+++ Automation/src/test/java/stepDefinitions/stepDefinition.java
@@ -0,0 +1,16 @@
+package stepDefinitions;
+import cucumber.api.*;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+public class stepDefinition {
+
+	@Given("User is on NetBanking landing page")
+	public void UserIsOnNetBankingLandingPage() {
+		
+	}
+	@When("User login into application with username and password")
+	public void User_login_into_application_with_username_and_password() {
+		
+	}
+	
+}
