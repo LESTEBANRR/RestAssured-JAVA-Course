17440a9273209208bdfb4dea54aa4eaa8944a264
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-15 16:50:02 -0500

    Clase 97

--- Automation/src/test/java/features/Login.feature
+++ Automation/src/test/java/features/Login.feature
@@ -4,10 +4,10 @@ Scenario: Home Page default login
 Given User is on NetBanking landing page
 When User login into application with username "jin" and password "1234"
 Then Home page is populated
-And Cards are displayed
+And Cards displayed are "true"
 
 Scenario: Home Page default login 2
 Given User is on NetBanking landing page
-When User login into application with username "john" and password "4321
+When User login into application with username "john" and password "4321"
 Then Home page is populated
-And Cards are not displayed
+And Cards displayed are "false"
--- Automation/src/test/java/stepDefinitions/stepDefinition.java
+++ Automation/src/test/java/stepDefinitions/stepDefinition.java
@@ -10,14 +10,15 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 public class stepDefinition {
 
+	@When("^User login into application with username \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_login_into_application_with_username_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+        System.out.println(strArg1);
+        System.out.println(strArg2);
+    }
+	
 	@Given("^User is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
         System.out.println("Step 1");
-    }
-
-    @When("^User login into application with username and password$")
-    public void user_login_into_application_with_username_and_password() throws Throwable {
-    	System.out.println("Step 2");
     }
 
     @Then("^Home page is populated$")
@@ -25,9 +26,9 @@ public class stepDefinition {
     	System.out.println("Step 3");
     }
 
-    @And("^Cards are displayed$")
-    public void cards_are_displayed() throws Throwable {
-    	System.out.println("Step 4");
+    @And("^Cards displayed are \"([^\"]*)\"$")
+    public void cards_displayed_are_something(String strArg1) throws Throwable {
+        System.out.println(strArg1);
     }
 
 }
