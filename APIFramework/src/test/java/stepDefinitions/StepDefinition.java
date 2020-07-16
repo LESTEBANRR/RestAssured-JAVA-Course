963eebfbd3afbda1e5d39be4d33364ec816f515a
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-16 14:02:52 -0500

    Clase 109 Enum Class

--- APIFramework/src/test/java/stepDefinitions/StepDefinition.java
+++ APIFramework/src/test/java/stepDefinitions/StepDefinition.java
@@ -10,6 +10,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import resources.APIResources;
 import resources.TestDataBuild;
 import resources.Utils;
 
@@ -32,10 +33,11 @@ public class StepDefinition extends Utils {
 	}
 
 	@When("^User calls \"([^\"]*)\" API with POST HTTP request$")
-	public void user_calls_something_api_with_post_http_request(String strArg1) throws Throwable {
-
+	public void user_calls_something_api_with_post_http_request(String resource) throws Throwable {
+		APIResources resourcdeAPI=APIResources.valueOf(resource);
+		
 		resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-		response = res.when().post("/maps/api/place/add/json").then().spec(resspec).extract().response();
+		response = res.when().post(resourcdeAPI.getResource()).then().spec(resspec).extract().response();
 
 	}
 
