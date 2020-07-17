165b54c5c7849ab26e5347e7c2f08506c797e347
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-17 11:00:16 -0500

    Clase 113

--- APIFramework/src/test/java/features/placeValidations.feature
+++ APIFramework/src/test/java/features/placeValidations.feature
@@ -6,9 +6,15 @@ Feature: Validating Place API's
     Then The API call is success with status code "200"
     And "status" in response body is "OK"
     And "scope" in response body is "APP"
-    And Verify place_Id created maps to "name" = <name> using "getPlace" API	
+    And Verify place_Id created maps to "name" = <name> using "getPlace" API
 
     Examples: 
       | name    | language | address  |
       | AAHome  | English  | World01  |
-      | BBHouse | Spanish  | Sea cost |
+     #| BBHouse | Spanish  | Sea cost |
+
+  Scenario: Verify if delete place functionality is working
+    Given DeletePLace payload
+    When User calls "deletePlace" API with "POST" HTTP request
+    Then The API call is success with status code "200"
+    And "status" in response body is "OK"
--- APIFramework/src/test/java/stepDefinitions/StepDefinition.java
+++ APIFramework/src/test/java/stepDefinitions/StepDefinition.java
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 public class StepDefinition extends Utils {
 
-	String place_id;
+	static String place_id;
 	JsonPath js;
 	RequestSpecification res;
 	ResponseSpecification resspec;
@@ -30,17 +30,25 @@ public class StepDefinition extends Utils {
 	@Given("^AddPlace Payload with (.+) (.+) and (.+)$")
 	public void addplace_payload_with_and(String name, String language, String address) throws IOException {
 		res = given().spec(requestSpecification()).body(data.addPlacePayload(name, language, address));
+		System.out.println("AddPlace Json Body Correctly Created");
+	}
+
+	@Given("^DeletePLace payload$")
+	public void deleteplace_payload() throws Throwable {
+		res = given().spec(requestSpecification()).body(data.deletePlacePayload(place_id));
+		System.out.println("deletePlace Json Body Correctly Created");
 	}
 
 	@When("^User calls \"([^\"]*)\" API with \\\"([^\\\"]*)\\\" HTTP request$")
 	public void user_calls_some_api_with_some_method_http_request(String resource, String method) throws IOException {
-		
-		APIResources resourcdeAPI=APIResources.valueOf(resource);	
+
+		APIResources resourcdeAPI = APIResources.valueOf(resource);
+		System.out.println(resourcdeAPI.getResource());
 		resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-		
-		if(method.equalsIgnoreCase("POST")) {			
+		System.out.println("\tRequesting to "+resourcdeAPI+" API ...");
+		if (method.equalsIgnoreCase("POST")) {
 			response = res.when().post(resourcdeAPI.getResource());
-		}else if (method.equalsIgnoreCase("GET")) {
+		} else if (method.equalsIgnoreCase("GET")) {
 			response = res.when().get(resourcdeAPI.getResource());
 		}
 	}
@@ -48,19 +56,22 @@ public class StepDefinition extends Utils {
 	@Then("^The API call is success with status code \\\"([^\\\"]*)\\\"$")
 	public void the_api_call_is_success_with_status_code_200(int code) throws Throwable {
 		assertEquals(response.getStatusCode(), code);
+		System.out.println("\t\tRequest Succesfully Done");
 	}
 
 	@And("^\"([^\"]*)\" in response body is \"([^\"]*)\"$")
 	public void something_in_response_body_is_something(String key, String value) throws Throwable {
 		assertEquals(getJsonPath(response, key), value);
+		System.out.println("\t\t\tThe value in the key "+key+" is equals to "+value);
 	}
-	
+
 	@And("^Verify place_Id created maps to \"([^\"]*)\" = (.+) using \"([^\"]*)\" API$")
-    public void verify_placeid_created_maps_to_some_key_using_some_api(String key ,String value, String resource) throws IOException {
-		place_id=getJsonPath(response, "place_id");
-		res = given().spec(requestSpecification())
-				.queryParam("place_id",place_id);
-		user_calls_some_api_with_some_method_http_request(resource,"GET");
+	public void verify_placeid_created_maps_to_some_key_using_some_api(String key, String value, String resource)
+			throws IOException {
+		place_id = getJsonPath(response, "place_id");
+		res = given().spec(requestSpecification()).queryParam("place_id", place_id);
+		user_calls_some_api_with_some_method_http_request(resource, "GET");
 		assertEquals(value, getJsonPath(response, key));
-    }
+		System.out.println("Place ID correctly verifired");
+	}
 }
